700af6657ca39cbd91c572018406eace26d50bd1
Nikita-Vikulov <n.vikulov92@yandex.ru>
2022-04-16 13:21:52 +0300

    dev

---
+++ app/src/main/java/com/example/caranimation/MainPresenter.kt
@@ -0,0 +1,28 @@
+package com.example.caranimation
+
+class MainPresenter {
+
+    private var view: MainView? = null
+    private var carPosition: CarAnimationState = CarAnimationState.Left
+
+    fun bind(activity: MainActivity) {
+        this.view = activity
+    }
+
+    fun unbind() {
+        this.view = null
+    }
+
+    fun onButtonClicked(): CarAnimationState {
+        carPosition = when (carPosition) {
+            CarAnimationState.Start -> CarAnimationState.Top
+            CarAnimationState.Top -> CarAnimationState.Right
+            CarAnimationState.Right -> CarAnimationState.Bottom
+            CarAnimationState.Bottom -> CarAnimationState.Left
+            CarAnimationState.Left -> CarAnimationState.Top
+        }
+
+        return carPosition
+    }
+
+}
